58940a388bef931c8b03ba1f907fa77b47b4d30b
Fabrizio Gasparini <makerfaffa@gmail.com>
2024-06-28 16:22:15 +0200

    Debug CORS (again)

--- app/src/api/axios.js
+++ app/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: ''/*'http://cors-anywhere.herokuapp.com/https://berealapi.fly.dev',
+    baseURL: 'http://cors-anywhere.herokuapp.com/https://berealapi.fly.dev',
     headers: {
-        'origin': 'http://localhost:5173/'
-    }*/
+        'origin': 'https://fabriziogasparini.github.io/BeFake'
+    }
 })
