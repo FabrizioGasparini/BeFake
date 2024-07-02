9021ab4d6ee0139e1d44d9114c820aabddbdfdef
Fabrizio Gasparini <makerfaffa@gmail.com>
2024-07-02 23:12:24 +0200

    Updated HashRouter

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoutes from './utils/PrivateRoute';
 import Home from './pages/Home'
 import Login from './pages/Login';
